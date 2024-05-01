e2ab93a8304ea3e018621bda33d7d6ba75b303ee
YOGEESH K P <148324344+yogee555@users.noreply.github.com>
2024-05-01 12:35:01 +0530

    Add files via upload

---
+++ if_cond.py
@@ -0,0 +1,6 @@
+# if condition
+a = 10                       # a is store 1 value
+b = 15                       # b is store 1 value
+if a < b:                    # a & b a is less than b
+    print('b is big')        # print enter function
+    print('b value is', b)   # print enter function and print b value
